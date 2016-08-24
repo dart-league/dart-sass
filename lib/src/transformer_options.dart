5d589410f50c2452b011b96c7189451dad7184a5
Luis Vargas <luisvargastijerino@gmail.com>
2016-08-24 01:16:59 -0400

    remove `entry_points` option

--- lib/src/transformer_options.dart
+++ lib/src/transformer_options.dart
@@ -2,8 +2,6 @@ part of sass.transformer;
 
 /// Class to be used to parse sass transformer options coming from pubspec.yaml file
 class TransformerOptions {
-  /// entry_point: web/builder.sass - main file to build or [file1.sass, ...,fileN.sass]
-  final List<String> entryPoints;
 
   /// include_path: /lib/sassIncludes - variable and mixims files
   final List<String> includePaths;
@@ -19,7 +17,6 @@ class TransformerOptions {
   final bool copySources;
 
   TransformerOptions({
-    this.entryPoints,
     this.includePaths,
     this.output,
     this.executable,
@@ -41,29 +38,14 @@ class TransformerOptions {
       return value ?? defaultValue;
     }
 
-    List<String> readEntryPoints(entryPoint, entryPoints) {
-      List<String> result = [];
-      List<String> value;
-
-      value = readStringList(entryPoint);
-      if (value != null) result.addAll(value);
-
-      value = readStringList(entryPoints);
-      if (value != null) result.addAll(value);
-
-      //if (result.length < 1) print('$INFO_TEXT No entry_point supplied. Processing *.sass and *.html.');
-      return result;
-    }
-
     return new TransformerOptions (
-        entryPoints: readEntryPoints(configuration['entry_point'], configuration['entry_points']),
         includePaths: readStringList(configuration['include_paths'], []),
         output: config('output', ''),
         executable: config('executable', (Platform.operatingSystem == "windows" ? "sass.bat" : "sass")),
         style: config("style", null),
         compass: config("compass", false),
-        lineNumbers: config("line-numbers", false),
-        copySources: config("copy-sources", false)
+        lineNumbers: config("line_numbers", false),
+        copySources: config("copy_sources", false)
     );
   }
 }
--- lib/transformer.dart
+++ lib/transformer.dart
@@ -8,20 +8,12 @@ import 'dart:io';
 
 part 'package:sass/src/transformer_options.dart';
 
-part 'package:sass/src/entry_points.dart';
-
 /// Transformer used by `pub build` and `pub serve` to convert Sass-files to CSS.
 class SassTransformer extends AggregateTransformer {
   SassTransformer.asPlugin(BarbackSettings settings) :
-        options = new TransformerOptions.parse(settings.configuration) {
-    entryPoints = new EntryPoints();
-    entryPoints.addPaths(options.entryPoints);
-    entryPoints.assureDefault(['*.sass', '*.scss']);
-  }
+        options = new TransformerOptions.parse(settings.configuration);
 
   final TransformerOptions options;
-
-  EntryPoints entryPoints;
 
   // Only process assets where the extension is ".scss" or ".sass".
   classifyPrimary(AssetId id) =>
@@ -35,7 +27,7 @@ class SassTransformer extends AggregateTransformer {
 
       // files excluded of entry_points are not processed
       // if user don't specify entry_points, the default value is all '*.sass' and '*.html' files
-      if (!entryPoints.check(id.path) || basename(id.path).startsWith('_')) {
+      if (basename(id.path).startsWith('_')) {
         // if asset is not an entry point it wild be consumed
         // (this is to no output scss files in build folder)
         return new Future(() => transform.consumePrimary(id));
