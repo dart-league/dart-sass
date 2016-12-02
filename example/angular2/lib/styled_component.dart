7549b40b307f402fb91daf5db1fcb91ef0693dd1
HVDK <hvdkintersentia@Hanss-MacBook-Pro.local>
2016-12-02 11:10:23 +0100

    Adds Angular2-based example

---
+++ example/angular2/lib/styled_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'styled-component',
+    styleUrls: const ['styled_component.css'],
+    templateUrl: 'styled_component.html')
+class StyledComponent {
+
+  StyledComponent();
+
+}
---
+++ example/angular2/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:dart_sass_angular2/styled_component.dart';
+
+main() {
+  bootstrap(StyledComponent);
+}
